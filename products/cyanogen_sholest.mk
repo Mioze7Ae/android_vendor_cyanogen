a5af1a1078e7d601d937a2912ffd55bc5288bf4a
Mioze7Ae <Mioze7Ae@gmail.com>
2011-08-31 13:05:44 -0400

    Correct include for froyo (common_full is gingerbread)
    Change-Id: Ie54fdefa6667a46a030145f238072cd14e424db3

--- products/cyanogen_sholest.mk
+++ products/cyanogen_sholest.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/motorola/sholest/sholest.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+$(call inherit-product, vendor/cyanogen/products/common.mk)
 
 # Include GSM stuff
 #$(call inherit-product, vendor/cyanogen/products/gsm.mk)
